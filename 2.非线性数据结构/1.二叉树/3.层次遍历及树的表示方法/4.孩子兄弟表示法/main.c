849c21198cdac27d9eacf969e9bd6e50cba0039a
lcs <515478826@qq.com>
2019-06-05 15:09:39 +0800

    woqu

--- 2.非线性数据结构/1.二叉树/3.层次遍历及树的表示方法/4.孩子兄弟表示法/main.c
+++ 2.非线性数据结构/1.二叉树/3.层次遍历及树的表示方法/4.孩子兄弟表示法/main.c
@@ -3,88 +3,45 @@
 #define MAX_SIZE 20
 #define TElemType char
 //孩子表示法
-typedef struct CTNode
+typedef struct CSNode
 {
-    int child; //链表中每个结点存储的不是数据本身，而是数据在数组中存储的位置下标
-    struct CTNode *next;
-} ChildPtr;
-typedef struct
-{
-    TElemType data; //结点的数据类型
-    ChildPtr *head; //孩子链表的头指针
-} CTBox;
-typedef struct
-{
-    CTBox nodes[MAX_SIZE]; //存储结点的数组
-    int n, r;              //结点数量和树根的位置
-} CTree;
+    char data;
+    struct CSNode *next, *nextBrother;
+} CSNode, CSTree;
+
 //孩子表示法存储普通树
-CTree initTree(CTree tree)
+void createTree(CSTree *t)
 {
-    tree.n = 4;
+    // 初始化根节点
+    // t = (CSTree *)malloc(sizeof(t));
+    t->data = 'R';
 
-    tree.nodes[0].data = 'R';
-    tree.nodes[0].head = (ChildPtr *)malloc(sizeof(ChildPtr));
-    tree.nodes[0].head->next = NULL;
+    t->next = (CSTree *)malloc(sizeof(t->next));
+    t->next->data = 'A';
 
-    ChildPtr *p = tree.nodes[0].head;
-    for (int j = 0; j < 3; j++)
-    {
-        ChildPtr *newEle = (ChildPtr *)malloc(sizeof(ChildPtr));
-        newEle->next = NULL;
-        newEle->child = j + 1;
-        p->next = newEle;
-        p = p->next;
-    }
+    t->next->next = (CSTree *)malloc(sizeof(t));
+    t->next->next->data = 'B';
 
-    tree.nodes[1].data = 'A';
-    tree.nodes[1].head = (ChildPtr *)malloc(sizeof(ChildPtr));
-    tree.nodes[1].head->next = NULL;
-
-    tree.nodes[2].data = 'B';
-    tree.nodes[2].head = (ChildPtr *)malloc(sizeof(ChildPtr));
-    tree.nodes[2].head->next = NULL;
-
-    tree.nodes[3].data = 'C';
-    tree.nodes[3].head = (ChildPtr *)malloc(sizeof(ChildPtr));
-    tree.nodes[3].head->next = NULL;
-
-    // tree.nodes[4].data = 'D';
-    // tree.nodes[4].head = (ChildPtr *)malloc(sizeof(ChildPtr));
-    // tree.nodes[4].head->next = NULL;
-
-    return tree;
+    t->next->nextBrother = (CSTree *)malloc(sizeof(t));
+    t->next->nextBrother->data = 'C';
 }
 
-void findKids(CTree tree, char a)
+void findChild(CSTree *t, char data)
 {
-    int hasKids = 0;
-    for (int i = 0; i < tree.n; i++)
+    CSNode *temp = t;
+    while (temp != NULL)
     {
-        if (tree.nodes[i].data == a)
+        if (t->data != data)
         {
-            ChildPtr *p = tree.nodes[i].head->next;
-            while (p)
-            {
-                hasKids = 1;
-                printf("%c ", tree.nodes[p->child].data);
-                p = p->next;
-            }
-            break;
+            temp = temp->next;
         }
     }
-    if (hasKids == 0)
-    {
-        printf("此节点为叶子节点");
-    }
+    return;
 }
 
 int main()
 {
-    CTree tree;
-    tree = initTree(tree);
-    //默认数根节点位于数组notes[0]处
-    tree.r = 0;
-    printf("找出节点 R 的所有孩子节点：");
-    findKids(tree, 'R');
+    CSTree *t = (CSTree *)malloc(sizeof(t));
+    createTree(t);
+    PreOrderTraverse(t);
 }
